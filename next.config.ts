a2e9d42ac38c6c91fac75210eaa27d1d4e39d706
Natalia de Souza Carvalho Correa <nataliacorrea@Natalias-MacBook-Air.local>
2026-02-21 13:19:44 -0500

    force rebuild without eslint

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,3 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -8,4 +7,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
